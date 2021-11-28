8fa623992d8e7a34e6a2cbcadd7c240f1f8d9035
jp <jpso.guerreiro@gmail.com>
2021-11-28 14:48:44 -0300

    Do stuff

--- run.js
+++ run.js
@@ -1,1 +1,23 @@
 const prompt = require("prompt-sync")
+const chalk = require("chalk")
+const path = require('path');
+const warn = function(message, color){
+  if (!color) color = red
+  console.log(chalk[color](message))
+}
+
+
+if (path.existsSync("/data")) { // or fs.existsSync
+    warn("WARNING: Folder \"data\" will be erased and rebuilt if you proceed")
+    const answer = prompt("Would you like to proceed? (S/N)")
+    if (String(answer).toUpperCase=="S"){
+      //delete
+      console.log("Done")
+    } else if (String(answer).toUpperCase=="N"){
+      console.log("Goodbye!")
+      exit(0)
+    }else{
+      warn("Not a valid answer. Exiting...")
+      exit(1)
+    }
+}
