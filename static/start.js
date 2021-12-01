dc6ea921e65cb5b9a9f35992759878d8c31642dd
jp <jpso.guerreiro@gmail.com>
2021-12-01 16:20:38 -0300

    .

--- static/start.js
+++ static/start.js
@@ -2,19 +2,13 @@ window.onload = function(){
 
 let upbutton = document.getElementById("upb")
 let dbutton = document.getElementById("downb")
-let frame = document.getElementById("frame")
+
 console.log("Script included.")
 upbutton.onclick = function(){
-    if (frame.src != "uploadframe.html") frame.src = "uploadframe.html";
-    window.document.title = "Upload something"
+   
 }
 dbutton.onclick =function(){
-    if (frame.src != "downframe.html") frame.src = "downframe.html"
-    window.document.title = "Download something"
+   
 }
-frame.onload = function(){
-    if (frame.src === "uploadframe.html"){
-        document.getElementById("iframeid").contentWindow.code = upcode;
-    }
-}
+
 }
